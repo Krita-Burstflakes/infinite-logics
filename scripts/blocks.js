4bdabdeeab646fe82f10f48fa2b6b636ebadf11e
Krita <130148398+Krita-Burstflakes@users.noreply.github.com>
2024-03-25 21:03:29 +0600

    V3

---
+++ scripts/blocks.js
@@ -0,0 +1,21 @@
+const infiniteProcessor = extend(LogicBlock, "infinite-processor", {});
+
+const armoredSwitch = extend(SwitchBlock, "armored-switch", {});
+
+const infiniteMessage = extend(MessageBlock, "infinite-message", {});
+
+const infiniteMemoryCell = extend(MemoryBlock, "infinite-memory-cell", {});
+
+const sorterDisplay = extend(Sorter, "sorterDisplay", {});
+
+const framelessDisplay = extend(LogicDisplay, "frameless-display", {});
+
+const largeFramelessDisplay = extend(LogicDisplay, "large-frameless-display", {});
+
+const boltPlayer = extend(PowerTurret, "bolt-player", {});
+
+const flamePlayer = extend(PowerTurret, "flame-player", {});
+
+const sparkPlayer = extend(PowerTurret, "spark-player", {});
+
+const godlyProcessor = extend(LogicBlock, "godly-processor", {});
---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+const mod = Vars.mods.locateMod("infinite-logics");
+
+require("blocks")
